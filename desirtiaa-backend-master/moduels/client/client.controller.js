46ebe8faebf76819ef88d665364af786eefe5652
ali-hatem <ali.h.010900@gmail.com>
2026-05-26 15:54:59 +0300

    Delete desirtiaa-backend-master directory

--- desirtiaa-backend-master/moduels/client/client.controller.js
+++
@@ -1,3 +0,0 @@
-export const getAllClients = (req,res)=>{
-    res.send("hello")
-}
--- desirtiaa-backend-master/moduels/client/client.routes.js
+++
@@ -1,5 +0,0 @@
-import express from 'express'
-import { getAllClients } from './client.controller.js'
-const clientRouter = express.Router()
-
-clientRouter.get('/',getAllClients)
